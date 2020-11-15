c11b97111c6de69e019ffd308cd4a8bd18b5a904
unknown <joao.je.nisa@gmail.com>
2020-11-15 18:40:20 +0000

    Another test to Travis CI

--- basic_calculator.py
+++ basic_calculator.py
@@ -9,7 +9,7 @@ class basic_calculator:
         Addition operation
         '''
 
-        return self.num1 + self.num2
+        return self.num1 + self.num2 + 3
 
     def subtract(self):
         '''
